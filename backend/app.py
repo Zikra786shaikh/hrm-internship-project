e657b58ef756c04daa75b45ccbe1af3ffe1e2f91
Bilal <bilalsk.777999@gmail.com>
2026-03-16 14:21:42 +0530

    Tested add department API using Thunder Client

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from db import db, cursor
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HRM System Backend Running"
+    return "HRM Backend Running"
+
+
+@app.route("/add_department", methods=["POST"])
+def add_department():
+    data = request.json
+
+    dept_name = data["dept_name"]
+    description = data["description"]
+
+    query = "INSERT INTO department (dept_name, description) VALUES (%s, %s)"
+    values = (dept_name, description)
+
+    cursor.execute(query, values)
+    db.commit()
+
+    return jsonify({"message": "Department added successfully"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
